a85c78af4a8b5d3890379e241129d7bdb11c87a3
Techatonic <techatonicgames@gmail.com>
2023-07-29 23:29:46 +0100

    Update generate_pdf.py

---
+++ animal_quotes.py
@@ -0,0 +1,69 @@
+animal_quotes = {
+    "hibbi": [
+        "Sudoku solving is taught at school in Hibbiland"
+    ],
+    "ginger": [
+        "I think row 7 column 3 is a 2"
+    ],
+    "lud": [
+        "If you buy me some hay, I'll help you out"
+    ],
+    "giraffy": [
+        "You might not be my mama but I still love you!"
+    ],
+    "rain": [
+        "The sudoku isn't purple so I can't help you, sorry"
+    ],
+    "stanley": [
+        "I'm on my way! I'll be there in two weeks :)"
+    ],
+    "lollie": [
+        "Top o' the mornin' to ya! Hope you're well",
+        "Well hello there. I'm really looking forward to seeing you again. I've missed you so much!"
+    ],
+    "florence": [
+        "Hello! I can't help but I can sit pretty and watch"
+    ],
+    "the_machine": [
+        "EEEEEEEAAAAAAAAAAAAAAAAARRRRGGGGGGGHHHHHHH"
+    ],
+    "hiphop": [
+        "Hewwoooo!"
+    ],
+    "zippy": [
+        ""
+    ],
+    "goatin": [
+        "When you're done, can I use the book as a perch??"
+    ],
+    "monochrome": [
+        ""
+    ],
+    "dmd": [
+        "Ducky McDuckface says QUACK"
+    ],
+    "drew_duckymore": [
+        "Quack Quack"
+    ],
+    "winston": [
+        "Winston!"
+    ]
+}
+animal_names = {
+    'hibbi': 'Hibbi',
+    'ginger': 'Ginger',
+    'lud': 'Lud',
+    'giraffy': 'Giraffy',
+    'rain': 'Rain',
+    'stanley': 'Stanley',
+    'lollie': 'Lollie',
+    'florence': 'Florence',
+    'the_machine': 'The Machine',
+    'hiphop': 'Hiphop',
+    'zippy': 'Zippy',
+    'goatin': 'Goatin',
+    'monochrome': 'Monochrome',
+    'dmd': 'DmD',
+    'drew_duckymore': 'Drew Duckymore',
+    'winston': 'Winston'
+}
--- generate_pdf.py
+++ generate_pdf.py
@@ -1,7 +1,20 @@
 import os
+import random
 
+from reportlab.lib import colors
+from reportlab.lib.enums import TA_CENTER
 from reportlab.lib.pagesizes import A5
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.utils import simpleSplit
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
+from reportlab.platypus import Paragraph
+
+from animal_quotes import animal_quotes, animal_names
+
+font_name = 'garamond'
+font_path = 'garamond.ttf'
 
 
 def add_images_to_pdf(image_paths, output_filename):
@@ -25,16 +38,57 @@ def add_images_to_pdf(image_paths, output_filename):
         c.setFont('Helvetica', 12)
         c.drawString(sudoku_number_x, sudoku_number_y, sudoku_number_text)
 
+        # Add blue border
+        style = getSampleStyleSheet()["Normal"]
+
+        # Create a custom ParagraphStyle
+        animal_quote_style = ParagraphStyle(name='Normal_CENTER',
+                                            parent=style,
+                                            fontName=font_name,
+                                            wordWrap='LTR',
+                                            alignment=TA_CENTER,
+                                            fontSize=12,
+                                            leading=13,
+                                            textColor=colors.black,
+                                            borderPadding=0,
+                                            leftIndent=0,
+                                            rightIndent=0,
+                                            spaceAfter=0,
+                                            spaceBefore=0,
+                                            splitLongWords=True,
+                                            spaceShrinkage=0.05,
+                                            )
+
+        # show animal quote
+        animal = random.choice(list(animal_quotes.keys()))
+        animal_quote_text = animal_names[animal] + ': "' + random.choice(animal_quotes[animal]) + '"'
+        para_text = Paragraph(animal_quote_text, style=animal_quote_style)
+        para_text.wrapOn(c, 200, 200)
+
+        # animal_quote_width = c.stringWidth(animal_quote_text, "Helvetica", 12)
+        animal_quote_width = para_text.width
+        animal_quote_x = (page_width - animal_quote_width) / 2
+        animal_quote_y = 60
+        c.setFont(font_name, 12)
+
+        # wrap_string(c, animal_quote_text, animal_quote_x, animal_quote_y, 400)
+        para_text.drawOn(c, animal_quote_x, animal_quote_y)
+        # c.drawString(animal_quote_x, animal_quote_y, animal_quote_text)
+
         c.showPage()
 
     c.save()
 
 
 def generate_pdf():
+    # Create custom pdf font
+    pdfmetrics.registerFont(TTFont(font_name, font_path))
+
     # Show all sudokus in folder
     directory = os.fsencode("./sudoku_imgs/")
     files = os.listdir(directory)
     directory_path = "./sudoku_imgs/"
     add_images_to_pdf([directory_path + os.fsdecode(file) for file in files], 'test.pdf')
+
 
 generate_pdf()
